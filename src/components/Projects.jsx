2e5443fc03dc67037a967539b0f800f2581fa62f
alextor26 <alextor2625@gmail.com>
2023-11-08 00:50:34 -0400

    Added more css

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,37 +1,62 @@
+import decodemeThumbnail from "../assets/thumbnails/DecodeMeThumbnail.gif";
+import MotekRewardsThumbnail from "../assets/thumbnails/MotekRewardsThumbnail.gif";
+import CookBookThumbnail from "../assets/thumbnails/CookBookThumbnail.png";
 const Projects = () => {
   return (
     <div id="Projects">
-      <h2>Projects</h2>
+      <h2 className="title-h2">Projects</h2>
       <div id="DecodeMe">
-        <img src="" alt="decodeme-thumbnail" />
         <div>
           <a href="https://alextor2625.github.io/decodeme/">
-            <h3>DecodeMe</h3>
-            <p>Implementation of the Caesar Cipher as a browser web game.</p>
+            <div className="project-title-container">
+              <h3 className="project-title">DecodeMe</h3>
+            </div>
+            <img
+              src={decodemeThumbnail}
+              alt="decodeme-thumbnail"
+              className="thumbnail"
+            />
           </a>
+          <p className="description">
+            Implementation of the Caesar Cipher as a browser web game.
+          </p>
         </div>
       </div>
       <div id="MotekRewards">
-        <img src="" alt="motekRewards-thumbnail" />
         <div>
           <a href="https://motekrewards.adaptable.app">
-            <h3>Motek Rewards</h3>
-            <p>IronHack project Webdev and UX/UI classes working together.</p>
+            <div className="project-title-container">
+              <h3 className="project-title">Motek Rewards</h3>
+            </div>
+            <img
+              src={MotekRewardsThumbnail}
+              alt="motekRewards-thumbnail"
+              className="thumbnail"
+            />
           </a>
+          <p className="description">
+            IronHack project Webdev and UX/UI classes working together.
+          </p>
         </div>
       </div>
       <div id="CookBook">
-        <img src="" alt="cookbook-thumbnail" />
         <div>
-          <a href="">
-            <h3>CookBook</h3>
-            <p>
-              Ironhack React Project, this app a Cookbook webapp. Allows users
-              to add cooking recipes into their cookbooks either by adding their
-              own recipe manually or adding other's recipes. This is the
-              Frontend of the webapp.
-            </p>
+          <a href="https://cookbookrecipe.netlify.app">
+            <div className="project-title-container">
+              <h3 className="project-title">CookBook</h3>
+            </div>
+            <img
+              src={CookBookThumbnail}
+              alt="cookbook-thumbnail"
+              className="thumbnail"
+            />
           </a>
+          <p className="description">
+            Ironhack React Project, this app a Cookbook webapp. Allows users to
+            add cooking recipes into their cookbooks either by adding their own
+            recipe manually or adding other's recipes. This is the Frontend of
+            the webapp.
+          </p>
         </div>
       </div>
     </div>
