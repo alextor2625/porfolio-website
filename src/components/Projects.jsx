19af421877384347e8718d8fe38a104e44a7946c
alextor26 <alextor2625@gmail.com>
2023-11-09 19:52:47 -0400

    Reorganized projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -8,6 +8,51 @@ const Projects = () => {
       <h2 className="title-h2" data-aos="fade-down">
         Projects
       </h2>
+      <div id="CookBook" className="project" data-aos="fade-left">
+        <div className="project-box">
+          <a
+            href="https://cookbookrecipe.netlify.app"
+            className="project-container"
+          >
+            <div>
+              <div className="project-title-container">
+                <h3>CookBook</h3>
+              </div>
+              <img
+                src={CookBookThumbnail}
+                alt="cookbook-thumbnail"
+                className="thumbnail"
+              />
+            </div>
+            <span className="description">
+              Ironhack React Project, this app is a Cookbook web app. Allows
+              users to add cooking recipes into their cookbooks either by adding
+              their own recipe manually or adding other's recipes.
+            </span>
+          </a>
+        </div>
+      </div>
+      
+      <div id="MotekRewards" className="project" data-aos="fade-right">
+        <div className="project-box">
+          <a
+            href="https://motekrewards.adaptable.app"
+            className="project-container"
+          >
+            <div className="project-title-container">
+              <h3>Motek Rewards</h3>
+            </div>
+            <span className="description">
+              IronHack project Webdev and UX/UI classes working together.
+            </span>
+            <img
+              src={MotekRewardsThumbnail}
+              alt="motekRewards-thumbnail"
+              className="thumbnail"
+            />
+          </a>
+        </div>
+      </div>
       <div id="DecodeMe" className="project" data-aos="fade-left">
         <div className="project-box">
           <a
@@ -30,50 +75,6 @@ const Projects = () => {
               at least 5 words before the timer runs out, you lose if you submit
               3 incorrect answers or if the timer runs out before you complete 5
               words.
-            </span>
-          </a>
-        </div>
-      </div>
-      <div id="MotekRewards" className="project" data-aos="fade-right">
-        <div className="project-box">
-          <a
-            href="https://motekrewards.adaptable.app"
-            className="project-container"
-          >
-            <div className="project-title-container">
-              <h3>Motek Rewards</h3>
-            </div>
-            <span className="description">
-              IronHack project Webdev and UX/UI classes working together.
-            </span>
-            <img
-              src={MotekRewardsThumbnail}
-              alt="motekRewards-thumbnail"
-              className="thumbnail"
-            />
-          </a>
-        </div>
-      </div>
-      <div id="CookBook" className="project" data-aos="fade-left">
-        <div className="project-box">
-          <a
-            href="https://cookbookrecipe.netlify.app"
-            className="project-container"
-          >
-            <div>
-              <div className="project-title-container">
-                <h3>CookBook</h3>
-              </div>
-              <img
-                src={CookBookThumbnail}
-                alt="cookbook-thumbnail"
-                className="thumbnail"
-              />
-            </div>
-            <span className="description">
-              Ironhack React Project, this app is a Cookbook web app. Allows
-              users to add cooking recipes into their cookbooks either by adding
-              their own recipe manually or adding other's recipes.
             </span>
           </a>
         </div>
